d22b3fab74dd97d1c7bf50df8b6bd0899a107469
Devender Singh Dhankhar <142579596+Devender0x1@users.noreply.github.com>
2024-09-24 18:55:41 +0530

    Error_handling.sol

--- Error_handling.sol
+++ Error_handling.sol
@@ -4,10 +4,8 @@ pragma solidity ^0.8.0;
 contract Division {
     
     function divide(uint256 numerator, uint256 denominator) public pure returns (uint256 result) {
-        // Check for division by zero
         require(denominator != 0, "Denominator cannot be zero");
-        
-        // Perform division
+    
         result = numerator / denominator;
     }
 }
